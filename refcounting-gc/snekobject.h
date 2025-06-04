2df6e59301b635907ef7853e7f364df0043ef96f
kwame-Owusu <alfredowusu595@gmail.com>
2025-06-04 12:52:16 +0100

    added function declarations for sneklang objects

--- refcounting-gc/snekobject.h
+++ refcounting-gc/snekobject.h
@@ -36,3 +36,10 @@ typedef struct SnekObject {
 } snek_object_t;
 
 
+snek_object_t *new_snek_integer(int value);
+snek_object_t *new_snek_float(float value);
+snek_object_t *new_snek_string(char *value);
+snek_object_t *new_snek_vector3(
+    snek_object_t *x, snek_object_t *y, snek_object_t *z
+);
+snek_object_t *new_snek_array(size_t size);
