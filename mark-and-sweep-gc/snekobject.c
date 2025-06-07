6e02f279b8a2190a9ef9b7900b3c39b25cf91d71
kwame-Owusu <alfredowusu595@gmail.com>
2025-06-07 11:14:20 +0100

    updated vm free function for stack and frames

---
+++ mark-and-sweep-gc/snekobject.c
@@ -0,0 +1,22 @@
+#include "snekobject.h"
+
+
+void snek_object_free(snek_object_t *obj) {
+  switch (obj->kind) {
+    case INTEGER:
+    case FLOAT:
+      break;
+    case STRING:
+      free(obj->data.v_string); 
+      break;
+    case VECTOR3: {
+      break;
+    }
+    case ARRAY: {
+      snek_array_t *array = &obj->data.v_array;
+      free(array->elements);
+      break;
+    }
+  }
+  free(obj);
+}
--- mark-and-sweep-gc/vm.c
+++ mark-and-sweep-gc/vm.c
@@ -1,34 +1,21 @@
 #include "vm.h"
-#include "snekobject.h"
 #include "stack.h"
+#include "snekobject.h"
 
-void vm_track_object(vm_t *vm, snek_object_t *obj) {
-  if(vm == NULL){
-    return;
+
+void vm_free(vm_t *vm) {
+  for(int i = 0; i < vm->frames->count; i++){
+    frame_free((snek_object_t *)vm->frames->data[i]);
   }
-  stack_push(vm->objects, obj);
+  stack_free(vm->frames);
   
-}
-
-
-void vm_frame_push(vm_t *vm, frame_t *frame) {
-  stack_push(vm->frames, frame);
-}
-
-frame_t *vm_new_frame(vm_t *vm) {
-  frame_t *frame = malloc(sizeof(frame_t));
-  if(frame == NULL){
-    return NULL;
+  for(int i = 0; i < vm->objects->count; i++){
+    snek_object_free((snek_object_t *)vm->objects->data[i]);
   }
-  frame->references = stack_new(8);
-  stack_push(vm->frames, frame);
-  return frame;
+  stack_free(vm->objects);
+  free(vm);
 }
 
-void frame_free(frame_t *frame) {
-  stack_free(frame->references);
-  free(frame);
-}
 
 vm_t *vm_new() {
   vm_t *vm = malloc(sizeof(vm_t));
@@ -41,11 +28,23 @@ vm_t *vm_new() {
   return vm;
 }
 
-void vm_free(vm_t *vm) {
-  for (int i = 0; i < vm->frames->count; i++) {
-    frame_free(vm->frames->data[i]);
-  }
-  stack_free(vm->frames);
-  stack_free(vm->objects);
-  free(vm);
+void vm_track_object(vm_t *vm, snek_object_t *obj) {
+  stack_push(vm->objects, obj);
+}
+
+void vm_frame_push(vm_t *vm, frame_t *frame) {
+  stack_push(vm->frames, frame);
+}
+
+frame_t *vm_new_frame(vm_t *vm) {
+  frame_t *frame = malloc(sizeof(frame_t));
+  frame->references = stack_new(8);
+
+  vm_frame_push(vm, frame);
+  return frame;
+}
+
+void frame_free(frame_t *frame) {
+  stack_free(frame->references);
+  free(frame);
 }
